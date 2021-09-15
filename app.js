f1b96e89503e791057d9bbac5ac2153575baf792
aditiverma271 <aditiv271@gmail.com>
2021-09-16 00:16:54 +0530

    console basics

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log("hello world");
+console.log("my first js code");
+console.warn("do not do this");
+
+console.error("be aware");
+console.warn("Aditi");
+console.log("ADITI");
+console.warn("ania");
