c1bbbe5936df4ad766a792eabd592f2096b57ef1
aditiverma271 <aditiv271@gmail.com>
2021-09-16 01:24:53 +0530

    data types

--- app.js
+++ app.js
@@ -6,3 +6,17 @@ console.error("be aware");
 console.warn("Aditi");
 console.log("ADITI");
 console.warn("ania");
+//DATA TYPES
+let age=15;
+console.log(age);
+console.log(typeof age);
+let Name="ania";
+console.log(Name);
+console.log(typeof Name);
+let brand;
+console.log(brand);
+let arr=[1,2,3,4];
+console.log(arr);
+arr.push("value");
+arr.push(false)
+console.log(arr);
